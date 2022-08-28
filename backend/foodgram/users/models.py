4147229d9706cadfd37b72c9f6e3b4dd6ca3428d
Михаил Глазов <glazov_52@mail.ru>
2022-08-28 14:39:52 +0300

    LowModels

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='адрес электронной почты',
+        blank=False,
+        unique=True,
+        max_length=254,
+    )
+    username = models.CharField(
+        verbose_name='логин',
+        max_length=150,
+        unique=True,
+        help_text='Не более 150 символов.',
+    )
+    first_name = models.CharField(verbose_name='имя', max_length=150)
+    last_name = models.CharField(verbose_name='фамилия', max_length=150)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
